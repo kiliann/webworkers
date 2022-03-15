1835500d51c99cff6ff92d071973abd8b2cd6e48
kiliann <wilian_kilian@hotmail.com>
2022-03-15 10:49:17 +0100

    hilos javaScript

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
 //myWorker.onmessage = (e)=> {console.log(parseInt(e.data))}
 let workerContador;
 let contador=0;
+//Aqui mandamos los datas extraidos al DOM y tenemos un contador que cuenta cuntas veces los extraemos
 function iniciarContador() {
     workerContador = new Worker('workes.js');
 
@@ -13,7 +14,7 @@ function iniciarContador() {
         document.getElementById('contador').innerText =contador;
     }
 }
-
+//Usamos para detener el contador y la extracion de los datos
 function detenerContador() {
     workerContador.terminate();
     workerContador = null;
--- workes.js
+++ workes.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const urlApi = "https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new";
+
+//Aqui hacemos las llamada a la api y extraemos los datos con un intervalo de 30 segundos.
 this.onmessage = setInterval((e)=>{
     fetch(urlApi)
         .then(reponse =>reponse.text())
